eef0fc389056b5e3a9d04293ff4673427aa29f89
Pedro Lucas <pedro99lucasom@gmail.com>
2020-11-05 01:28:40 -0300

    Implementação de redes específicas nos serviços e persistência dos e-mails no banco de dados postgres

--- app/sender.py
+++ app/sender.py
@@ -1,9 +1,25 @@
+import psycopg2
 from bottle import route, run, request
+
+DSN = 'dbname=email_sender user=postgres host=db password=postgres'
+SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+
+def register_message(assunto, mensagem):
+        conn = psycopg2.connect(DSN)
+        cur = conn.cursor()
+        cur.execute(SQL, (assunto, mensagem))
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        print('Mensagem registrada!')
 
 @route('/', method='POST')
 def send():
     assunto = request.forms.get('assunto')
     mensagem = request.forms.get('mensagem')
+
+    register_message(assunto, mensagem)
     return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
         assunto, mensagem
     )
